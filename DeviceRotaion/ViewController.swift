5666e4f3c33e18c4f8560273066416fa36c518a3
desktop <desktop@desktop.com>
2018-02-06 13:53:56 +0800

    demo finally

--- DeviceRotaion/ViewController.swift
+++ DeviceRotaion/ViewController.swift
@@ -16,21 +16,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageSingle: UIImageView!
     var picImageView: UIImageView!
 
-    @IBAction func doAction(_ sender: UIButton) {
-        UIView.animate(withDuration: 0.2, animations: {
+    @IBAction func doAction(_ sender: UIButton!) {
+        UIView.animate(withDuration: 0.1, animations: {
             //1st
             self.imageSingle.frame.origin.x += self.deviceWidth - self.imageSingle.frame.size.width
             self.imageSingle.alpha = 0.5
         }) { (isFinished) in
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.1, animations: {
                 //2nd
                 self.imageSingle.frame.origin.y += self.deviceHeight - self.imageSingle.frame.size.height
             }, completion: { (isFinished) in
-                UIView.animate(withDuration: 0.2, animations: {
+                UIView.animate(withDuration: 0.1, animations: {
                     //3rd
                     self.imageSingle.frame.origin.x = 0
                 }, completion: { (isFinished) in
-                    UIView.animate(withDuration: 0.3, animations: {
+                    UIView.animate(withDuration: 0.1, animations: {
                         //4th
                         self.imageSingle.frame.origin.y = 0
                         self.imageSingle.alpha = 1.0
@@ -44,6 +44,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //trait collection
+        deviceHeight = UIScreen.main.bounds.size.height
+        deviceWidth = UIScreen.main.bounds.size.width
+        deviceScreen = UIScreen.main.bounds.size
         switch self.traitCollection.userInterfaceIdiom {
         case .phone:
             print("this is a phone trait")
@@ -58,27 +61,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
-        super.willTransition(to: newCollection, with: coordinator)
 
-        switch newCollection.horizontalSizeClass {
-        case .compact:
-            print("will transition horizontalSizeClass compact")
-        case .regular:
-            print("will transition horizontalSizeClass regular")
-        case .unspecified:
-            print("will transition horizontalSizeClass unspecified")
-        }
-
-        switch newCollection.verticalSizeClass {
-        case .compact:
-            print("will transition verticalSizeClass compact")
-        case .regular:
-            print("will transition verticalSizeClass regular")
-        case .unspecified:
-            print("will transition verticalSizeClass unspecified")
-        }
-    }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
@@ -86,6 +69,8 @@ class ViewController: UIViewController {
         deviceWidth = UIScreen.main.bounds.size.width
         deviceScreen = UIScreen.main.bounds.size
         print("viewDidLayoutSubviews .width = \(deviceScreen.width), .height= \(deviceScreen.height)")
+
+//        doAction(nil)
 
         if picImageView == nil {
             picImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: deviceWidth, height: deviceHeight))
@@ -96,18 +81,16 @@ class ViewController: UIViewController {
             imageSingle.image = UIImage(named: "landscape")
 
             picImageView.image = UIImage(named: "picLandscape")
-            picImageView.contentMode = .scaleAspectFill
-            picImageView.clipsToBounds = true
         case .portrait, .portraitUpsideDown:
             imageSingle.image = UIImage(named: "portrait")
 
             picImageView.image = UIImage(named: "picPortrait")
-            picImageView.contentMode = .scaleAspectFill
-            picImageView.clipsToBounds = true
         default:
             print("others")
         }
 
+        picImageView.contentMode = .scaleAspectFill
+        picImageView.clipsToBounds = true
         if picImageView != nil {
             picImageView.removeFromSuperview()
             picImageView.frame = CGRect(x: 0, y: 0, width: deviceWidth, height: deviceHeight)
@@ -117,6 +100,16 @@ class ViewController: UIViewController {
     }
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         print("width = \(size.width), height= \(size.height)")
+
+        deviceHeight = size.height
+        deviceWidth = size.width
+        deviceScreen = size
+//        coordinator.animate(alongsideTransition: { (context) in
+//            self.doAction(nil)
+//        }, completion: nil)
+        coordinator.animateAlongsideTransition(in: self.view, animation: { (context) in
+            self.doAction(nil)
+        }, completion: nil)
 
 //        imageSingle.image = UIImage(named: "portrait")
 
@@ -135,6 +128,28 @@ class ViewController: UIViewController {
 //            print("others")
 //        }
     }
+
+    //    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+    //        super.willTransition(to: newCollection, with: coordinator)
+    //
+    //        switch newCollection.horizontalSizeClass {
+    //        case .compact:
+    //            print("will transition horizontalSizeClass compact")
+    //        case .regular:
+    //            print("will transition horizontalSizeClass regular")
+    //        case .unspecified:
+    //            print("will transition horizontalSizeClass unspecified")
+    //        }
+    //
+    //        switch newCollection.verticalSizeClass {
+    //        case .compact:
+    //            print("will transition verticalSizeClass compact")
+    //        case .regular:
+    //            print("will transition verticalSizeClass regular")
+    //        case .unspecified:
+    //            print("will transition verticalSizeClass unspecified")
+    //        }
+    //    }
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .all // override var .allButUpsideDown (read only)
     }
